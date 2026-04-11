fd1a40a5c6e04f0cb6974f337d13807bab1df1b5
kaochin <kzk4289@gmail.com>
2026-04-11 16:58:01 +0900

    ルーティング

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// --- 【一般公開ページ】 ---
+Route::get('/', [ContactController::class, 'index']); Route::post('/confirm', [ContactController::class, 'confirm']); Route::post('/thanks', [ContactController::class, 'store']);
+
+// --- 【管理画面（ログイン必須）】 ---
+Route::middleware('auth')->group(function ()
+    {
+        Route::get('/admin', [ContactController::class, 'admin']); Route::get('/search', [ContactController::class, 'search']); Route::get('/reset', [ContactController::class, 'reset']); Route::delete('/delete', [ContactController::class, 'destroy']);
+        Route::get('/export', [ContactController::class, 'export']);
+    });
